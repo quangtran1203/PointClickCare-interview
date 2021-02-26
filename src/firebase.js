ceb7bec946e479196815bf712b388fb6730a302c
quangtran1203 <harrytran1232001@gmail.com>
2021-02-26 13:08:36 -0500

    final commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDI0R7kQXIOcRhdVgr7xwycGXdWRfwgW-w",
+    authDomain: "pointclickcare-int.firebaseapp.com",
+    projectId: "pointclickcare-int",
+    storageBucket: "pointclickcare-int.appspot.com",
+    messagingSenderId: "242723679711",
+    appId: "1:242723679711:web:1fc2776dfe89c2c3d6686a",
+    measurementId: "G-92N9JS1M87"
+  };
